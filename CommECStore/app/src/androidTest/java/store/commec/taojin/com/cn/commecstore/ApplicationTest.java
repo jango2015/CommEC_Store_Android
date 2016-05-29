8d204c5fad059080c4eac8c65b5ff5efde6bfc2b
kris.li <kris.li@iisigroup.com>
2016-05-29 23:46:46 +0800

    Project Initial.

---
+++ CommECStore/app/src/androidTest/java/store/commec/taojin/com/cn/commecstore/ApplicationTest.java
@@ -0,0 +1,13 @@
+package store.commec.taojin.com.cn.commecstore;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ CommECStore/app/src/main/java/store/commec/taojin/com/cn/commecstore/MainActivity.java
@@ -0,0 +1,13 @@
+package store.commec.taojin.com.cn.commecstore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
